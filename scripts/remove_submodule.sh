92e2cb542b5f2901d4d24ea01dd322d800e5aab2
William McCohen <william.mccohen@gmail.com>
2015-05-28 16:13:18 +0200

    fix minor

--- scripts/remove_submodule.sh
+++ scripts/remove_submodule.sh
@@ -2,8 +2,8 @@
 submodulepath=$1
 git config -f .git/config --remove-section submodule.$submodulepath
 git config -f .gitmodules --remove-section submodule.$submodulepath
+git add .gitmodules
 echo "Remove directory from index"
 git rm --cached $submodulepath
 rm -rf $submodulepath
 rm -rf .git/modules/$submodulepath
-git add .gitmodules
